d5d720543bd31a2d7b8132bb0c2b902956d36bb5
pasolre95kim <pasolre95kim@gmail.com>
2018-09-02 17:20:53 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,10 @@
 class Dog
   # code goes here
+  attr_reader :name #looks at instance variable name
+  attr_accessor :mood # can change its mood bc of accessor
+  def initialize(name) #can initialize a dog
+    @name = name
+    @mood = "nervous"
+
+  end
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,10 @@
 class Fish
   # code goes here
+    attr_reader :name #can't change its name
+    attr_accessor :mood
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+
+  end
 end
